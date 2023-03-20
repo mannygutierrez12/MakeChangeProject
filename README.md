55fde7b7990afdc0d4a31a77965b48541394db4a
mannygutierrez12 <iberiard@gmail.com>
2023-03-19 23:16:19 -0400

    Completed the README. md
    I completed the readme file as well cleaned the code.

--- README.md
+++ README.md
@@ -1,12 +1,34 @@
-# MakeChangeProject (Header)
+##MakeChangeProject
 
-## (Sub-headers)
+#How this project works.
+
+The idea behind creating this cash register machine is to assist employees in choosing the right amount as well as the highest possible denomination. For instance, if a purchase costs 59.75 cents, giving the customer 5 nickels and 40 1-dollar bills would be cumbersome. In this case, the program will prompt that the most efficient way to give the remaining money back to the customer would be 2 20-dollar bills and 1 quarter. We must emphasize that the largest denomination is the 20-dollar bill, and the smallest denomination would be 1 cent or better known as a penny.
+
+Each bill would use its respective denomination. For instance, a 20-dollar bill would be referred to as just a "20-dollar bill." The same happens for bills of 1, 5, and 10. On the other hand, any amount inferior would be referred to using colloquial American names. Please see the example below.
+
+25 cents = Quarter
+10 cents = Dime
+05 cents = Nickel
+01 cent = Penny
+At the end of the program, the user would be asked one time if he or she would like to perform any other calculation. If the user chooses "yes," the method would be recalled, and he or she can continue performing a calculation. If the user chooses "no," the program would prompt a goodbye message.
 
 #Technologies Used
 
 -Java
+--Used for the creation of methods, while loops, Math.round, Scanner, and primitive data types.
+
 -Eclipse
--Git 
+--Used as an IDE.
 
-## Lesson Learned
+-Git
+--Used as a repository compartment and to push/pull files.
 
+#Lessons Learned
+
+We learned how to use the knowledge acquired during the first week of class to put everything together and create a non-GUI application. I think the first version of this program would be the first step, but the program has lots of potential to improve. For instance, a loop could be created to encrypt a password that only authorized users can use.
+
+We could create a message that would pop up every time the machine is running low on a certain bill.
+
+We could create a while loop that keeps count of the money by creating a first count and comparing it with the last count, providing the user with the money available at a specific time.
+
+These are just small examples of things that might be possible, but the opportunities are endless.
--- src/MakeChange.java
+++ src/MakeChange.java
@@ -1,8 +1,121 @@
+import java.util.Scanner;
 
 public class MakeChange {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+
+		makeCalculations();
+		
+		
+		Scanner kb = new Scanner(System.in);
+		System.out.println("Do you want to do another calculation. Type (Y/y) or (N/n)?");
+		String calculateAgain = kb.next().toLowerCase();
+		if (calculateAgain.equals("y")) {
+			makeCalculations();
+		}
+
+		else {
+			System.out.println("See you next time!");
+		}
+
+	}
+
+	public static void makeCalculations() {
+		// The following variables handle the change based on the cost of the items and
+		// amount received from customer
+		Scanner kb = new Scanner(System.in);
+		
+		double price;
+		double customerPayment;
+		double totalChange;
+
+		// The following variable represent the amount a currency can go into a bill
+		// using a whole number
+		int billAmounts;
+
+		// The following boolean represents a on/off button
+
+		boolean stillOn = true;
+
+		System.out.println("How much does the item(s) cost?: ");
+		price = kb.nextDouble();
+		System.out.println("How much money did the customer give you?: ");
+		customerPayment = kb.nextDouble();
+		totalChange = customerPayment - price;
+		while (stillOn = true) {
+
+			if (totalChange < 0) {
+				System.out.println("Error message: Sorry, but you do not have enough money");
+				break;
+			}
+
+			else if (totalChange == 0) {
+				System.out.println("Exact amount. No change needed!");
+				break;
+			}
+
+			if (totalChange >= 20) {
+				billAmounts = (int) (totalChange / 20);
+				System.out.println(billAmounts + " Twenty Dollar Bill(s)");
+				totalChange = totalChange - (20 * billAmounts);
+				continue;
+			}
+
+			if (totalChange >= 10) {
+				billAmounts = (int) (totalChange / 10);
+				System.out.println(billAmounts + " Ten Dollar Bill(s)");
+				totalChange = totalChange - (10 * billAmounts);
+				continue;
+			}
+
+			if (totalChange >= 5) {
+				billAmounts = (int) (totalChange / 5);
+				System.out.println(billAmounts + " Five Dollar Bill(s)");
+				totalChange = totalChange - (5 * billAmounts);
+				continue;
+			}
+
+			if (totalChange >= 1) {
+				billAmounts = (int) (totalChange / 1);
+				System.out.println(billAmounts + " One Dollar Bill(s)");
+				totalChange = totalChange - (1 * billAmounts);
+				continue;
+			}
+
+			if (totalChange >= .25) {
+				billAmounts = (int) (totalChange / .25);
+				System.out.println(billAmounts + " quarter");
+				totalChange = totalChange - (.25 * billAmounts);
+				continue;
+			}
+
+			if (totalChange >= .10) {
+				billAmounts = (int) (totalChange / .10);
+				System.out.println(billAmounts + " dime");
+				totalChange = totalChange - (.10 * billAmounts);
+				continue;
+			}
+
+			if (totalChange >= .05) {
+				billAmounts = (int) (totalChange / .05);
+				System.out.println(billAmounts + " nickel");
+				totalChange = totalChange - (.05 * billAmounts);
+				continue;
+			}
+
+			if (totalChange >= .01) {
+				billAmounts = (int) Math.round((totalChange / .01));
+				System.out.println(billAmounts + " pennie(s)");
+				totalChange = Math.round(totalChange - (.01 * billAmounts));
+				break;
+
+			}
+
+			else {
+				stillOn = false;
+				break;
+			}
+		}
 
 	}
 
